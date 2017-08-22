58f5ea649718aebf13099c8b0936da11fa1cef9c
Chris Nady <nady.christopher@gmail.com>
2017-08-22 18:40:30 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ function updatePlaylist (playlist, name, title) {
 }
 
 function removeFromPlaylist (playlist, artistName) {
-  var list = playlist
+  var list = playlist;
   delete list.artistName;
   return list;
 }
